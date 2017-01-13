35ac2c62825645bf0504abdaf2a27e3c6f595287
Ian Kennedy <exposure@gmail.com>
2017-01-13 13:17:03 -0500

    cleanup commit before working on the work page.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,10 +9,8 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
-		<?php
-			the_content();
-		?>
-	</div><!-- .entry-content -->
+<article class="article--main">
+  <?php
+  the_content();
+  ?>
 </article><!-- #post-## -->
